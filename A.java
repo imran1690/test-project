ded99e99044416d606403e1250068d96b22de875
Imran Mohammed <Imran Mohammed>
2016-01-21 21:58:07 -0500

    Added in new branch

---
+++ A.java
@@ -0,0 +1,17 @@
+package com.pack1;
+
+public class A {
+
+	private int aa=5;
+	int b=10;
+	protected int c=20;
+	public int d=25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
+}
